667896cd33f4891c9d3cffa93a5f986fd1bf8d63
MasumAhmod <masumahmod332@gmail.com>
2025-01-05 00:48:13 +0600

    A - MEX Table

---
+++ MEX Table.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
+#define endl '\n'
+#define pb push_back
+#define vi vector<int>
+#define vii vector<long long>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define gcd __gcd
+#define MOD 1000000007
+
+void solve()
+{
+    ll n , m;
+    cin >> n >> m;
+
+    ll ans = max(n , m) + 1;
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ///cODE BY Masum
+    optimize();
+
+    //solve();
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
